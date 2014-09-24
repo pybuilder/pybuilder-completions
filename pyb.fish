e367b8ec0aebed32681b0c3b99b9146dbaaa219e
Maximilien Riehl <max@riehl.io>
2014-09-24 14:07:02 +0200

    suppress file completions

--- pyb.fish
+++ pyb.fish
@@ -2,26 +2,26 @@
 # auto completion for pybuilder
 # Project Options
 function completion
-complete -c pyb -l version --description 'show programs version number and exit'
-complete -c pyb -a -h --description 'show this help message and exit'
-complete -c pyb -a -t --description 'List tasks'
-complete -c pyb -l start-project --description 'Initialize a build descriptor and python project structure.'
-complete -c pyb -l verbose --description 'Enable verbose output'
-complete -c pyb -a -D --description '<project directory> Root directory to execute in'
-complete -c pyb -l project-directory --description '--project-directory=<project directory> Root directory to execute in'
-complete -c pyb -a -E --description '<environment> Activate the given environment for this build. Can be used multiple times'
-complete -c pyb -l environment --description '--environment=<environment> Activate the given environment for this build. Can be used multiple times'
-complete -c pyb -a -P --description '<property>=<value> Set/ override a property value'
+complete -c pyb -f -l version --description 'show programs version number and exit'
+complete -c pyb -f -a -h --description 'show this help message and exit'
+complete -c pyb -f -a -t --description 'List tasks'
+complete -c pyb -f -l start-project --description 'Initialize a build descriptor and python project structure.'
+complete -c pyb -f -l verbose --description 'Enable verbose output'
+complete -c pyb -f -a -D --description '<project directory> Root directory to execute in'
+complete -c pyb -f -l project-directory --description '--project-directory=<project directory> Root directory to execute in'
+complete -c pyb -f -a -E --description '<environment> Activate the given environment for this build. Can be used multiple times'
+complete -c pyb -f -l environment --description '--environment=<environment> Activate the given environment for this build. Can be used multiple times'
+complete -c pyb -f -a -P --description '<property>=<value> Set/ override a property value'
 # Output Options
-complete -c pyb -a -X --description 'Print debug messages'
-complete -c pyb -l debug --description 'Print debug messages'
-complete -c pyb -a -q --description 'Quiet mode; print only warnings and errors'
-complete -c pyb -l quiet --description 'Quiet mode; print only warnings and errors'
-complete -c pyb -a -Q --description 'Very quiet mode; print only errors'
-complete -c pyb -l very-quiet --description 'Very quiet mode; print only errors'
-complete -c pyb -a -C --description 'Disable colored output'
-complete -c pyb -l no-color --description 'Disable colored output'
-complete -c pyb -a -Qt --description 'generates Dynamicly tasks'
+complete -c pyb -f -a -X --description 'Print debug messages'
+complete -c pyb -f -l debug --description 'Print debug messages'
+complete -c pyb -f -a -q --description 'Quiet mode; print only warnings and errors'
+complete -c pyb -f -l quiet --description 'Quiet mode; print only warnings and errors'
+complete -c pyb -f -a -Q --description 'Very quiet mode; print only errors'
+complete -c pyb -f -l very-quiet --description 'Very quiet mode; print only errors'
+complete -c pyb -f -a -C --description 'Disable colored output'
+complete -c pyb -f -l no-color --description 'Disable colored output'
+complete -c pyb -f -a -Qt --description 'generates Dynamicly tasks'
 end
 # Dynamicly generated completion of pybuilder tasks
 
@@ -37,4 +37,4 @@ function task_generation
             echo $tasks[$number]
         end
 end
-complete -c pyb -a "(task_generation)" --description 'check task description with pyb -t'
+complete -c pyb -f -a "(task_generation)" --description 'check task description with pyb -t'
