4c180109bc4316e46015fe5bb09b89aa33f24674
mriehl <max@riehl.io>
2014-12-19 16:46:06 +0100

    fix release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os.path import expanduser
 if __name__ == '__main__':
     setup(
         name='pybuilder-completions',
-        version='0.2',
+        version='0.3',
         description='''shell and zsh completions for pybuilder''',
         long_description='''shell and zsh completions for pybuilder
 currently supported:
