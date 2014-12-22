547113933213157bb3ead76a056a1211831f29e8
mriehl <max@riehl.io>
2014-12-22 09:35:46 +0100

    factorize long_description

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ if __name__ == '__main__':
     setup(
         name='pybuilder-completions',
         version='0.4',
-        description='''shell and zsh completions for pybuilder''',
-        long_description='''shell and zsh completions for pybuilder
-currently supported:
-- fish shell v2
-- zsh
-''',
+        description='shell and zsh completions for pybuilder',
+        long_description=('shell and zsh completions for pybuilder\n',
+                          'currently supported:\n'
+                          '\t- fish shell v2\n'
+                          '\t- zsh\n'
+                          ),
         author="PyBuilder Team",
         author_email="max@riehl.io, marcel.wolf@me.com",
         license='MIT',
